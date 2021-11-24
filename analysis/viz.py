eef77c76e5e0499118c8bbbb4f568e71c5b54f30
srishti_gupta <srishti.gupta1@ashoka.edu.in>
2021-11-24 14:46:43 +0530

    Plotly issue

--- analysis/viz.py
+++ analysis/viz.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.figure_factory as ff
+from plotly.tools import FigureFactory as ff
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
